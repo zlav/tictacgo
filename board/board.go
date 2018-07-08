5d517dbe8980ad8826b66e3b3ba01c009ea8e380
zlav <zcjlavallee@gmail.com>
2018-07-07 20:21:05 -0700

    packages sorted out

---
+++ board/board.go
@@ -0,0 +1,7 @@
+package board
+
+import "fmt"
+
+func DoSomething() {
+	fmt.Println("Hello")
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+import "github.com/zlav/tictacgo/board"
+
+func main() {
+	fmt.Println("vim-go")
+	board.DoSomething()
+}
